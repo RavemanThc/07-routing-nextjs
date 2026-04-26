7b803388c828cba5bb79127a566a1b9478463657
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-04-26 19:32:37 +0300

    change building

--- components/NoteItem/NoteItem.tsx
+++ components/NoteItem/NoteItem.tsx
@@ -1,17 +1,16 @@
-import { Note } from "@/lib/api"
+import { Note } from "@/types/note";
 import Link from "next/link";
 
-
 type Props = {
-    item: Note;
+  item: Note;
 };
 
-const NoteItem = ({item}: Props) => {
-    return(
-        <li>
-            <Link href={`/notes/${item.id}`}>{item.title}</Link>
-        </li>
-    );
+const NoteItem = ({ item }: Props) => {
+  return (
+    <li>
+      <Link href={`/notes/${item.id}`}>{item.title}</Link>
+    </li>
+  );
 };
 
 export default NoteItem;
